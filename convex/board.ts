7a082183a2ff0690e013a7520035ebcc68f75748
Cristian Cirje <cirjecristian@icloud.com>
2024-03-16 16:38:10 +0100

    Fix: random board images

--- convex/board.ts
+++ convex/board.ts
@@ -3,16 +3,16 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
 const images = [
-  "/placeholders/1.svg",
-  "/placeholders/2.svg",
-  "/placeholders/3.svg",
-  "/placeholders/4.svg",
-  "/placeholders/5.svg",
-  "/placeholders/6.svg",
-  "/placeholders/7.svg",
-  "/placeholders/8.svg",
-  "/placeholders/9.svg",
-  "/placeholders/10.svg",
+  "/assets/placeholders/1.svg",
+  "/assets/placeholders/2.svg",
+  "/assets/placeholders/3.svg",
+  "/assets/placeholders/4.svg",
+  "/assets/placeholders/5.svg",
+  "/assets/placeholders/6.svg",
+  "/assets/placeholders/7.svg",
+  "/assets/placeholders/8.svg",
+  "/assets/placeholders/9.svg",
+  "/assets/placeholders/10.svg",
 ];
 
 export const create = mutation({
